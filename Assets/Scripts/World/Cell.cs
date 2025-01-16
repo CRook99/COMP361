4b22c038408c4e24683cdded6d5a294b10de7577
CRook99 <cjrook43@gmail.com>
2025-01-15 22:21:14 -0500

    Neighbour definition and computation

--- Assets/Scripts/World/Cell.cs
+++ Assets/Scripts/World/Cell.cs
@@ -4,11 +4,26 @@ public class Cell
 {
     public Vector2Int Position { get; private set; }
     public bool Walkable { get; set; }
+    
+    // Neighbours are defined clockwise, from N
+    // 0: N, 1: NE, 2: E etc.
+    public Cell[] Neighbours { get; set; }
+
+    public Cell N => Neighbours[0];
+    public Cell NE => Neighbours[1];
+    public Cell E => Neighbours[2];
+    public Cell SE => Neighbours[3];
+    public Cell S => Neighbours[4];
+    public Cell SW => Neighbours[5];
+    public Cell W => Neighbours[6];
+    public Cell NW => Neighbours[7];
 
     public Cell(Vector2Int position, bool walkable = true)
     {
         Position = position;
         Walkable = walkable;
+
+        Neighbours = new Cell[8];
     }
 }
 
--- Assets/Scripts/World/TacticsGrid.cs
+++ Assets/Scripts/World/TacticsGrid.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -19,16 +20,38 @@ namespace World
             _mapParser = GetComponent<MapParser>();
             
             _mapParser.ReadMap(out _cellMap, out _obstacles);
+            PrecomputeNeighbours();
         }
 
         public Cell GetCell(int x, int y)
         {
-            return _cellMap[new Vector2Int(x, y)];
+            if (x < 0 || y < 0) return null;
+
+            _cellMap.TryGetValue(new Vector2Int(x, y), out var cell);
+            return cell;
         }
 
         public Cell GetCell(Vector2Int xy)
         {
-            return _cellMap[xy];
+            if (xy.x < 0 || xy.y < 0) return null;
+
+            _cellMap.TryGetValue(xy, out var cell);
+            return cell;
+        }
+
+        private void PrecomputeNeighbours()
+        {
+            int[] xMap = { 0, 1, 1, 1, 0, -1, -1, -1 };
+            int[] yMap = { 1, 1, 0, -1, -1, -1, 0, 1 };
+            foreach (Cell cell in _cellMap.Values)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    Cell neighbour = GetCell(cell.Position.x + xMap[i], cell.Position.y + yMap[i]);
+                    if (neighbour != null)
+                        cell.Neighbours[i] = neighbour;
+                }
+            }
         }
     }
 }
