97c99b4192a03d691d5a861ad8b9275445eb02d8
CJRook43 <cjrook43@gmail.com>
2025-01-25 20:50:39 -0500

    Pass to PC

---
+++ Assets/Scripts/UI/EquipmentScreen.cs
@@ -0,0 +1,20 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+namespace UI
+{
+    public class EquipmentScreen : MonoBehaviour
+    {
+        public void OnClickBackButton()
+        {
+            Debug.Log("Return");
+            // Scene transition
+        }
+
+        public void OnClickCombatButton()
+        {
+            Debug.Log("Combat");
+            // Scene transition
+        }
+    }
+}
