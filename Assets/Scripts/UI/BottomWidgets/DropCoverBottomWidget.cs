454a1083b2695ddb8604cbd19f980108fb4d8cd8
CJRook43 <cjrook43@gmail.com>
2025-03-23 17:45:52 -0400

    Drop cover no effects

--- Assets/Scripts/UI/BottomWidgets/DropCoverBottomWidget.cs
+++ Assets/Scripts/UI/BottomWidgets/DropCoverBottomWidget.cs
@@ -1,3 +1,6 @@
+using System;
+using Managers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,24 +8,59 @@ namespace UI.BottomWidgets
 {
     public class DropCoverBottomWidget : SubBottomWidget
     {
-        [Space]
         [SerializeField] private Button backButton;
+        [SerializeField] private Button fireButton;
+        [SerializeField] private TextMeshProUGUI titleText;
+        [SerializeField] private TextMeshProUGUI descriptionText;
+
+        [Space] [SerializeField] private DropCoverManager dropCoverManager;
 
         protected override void Awake()
         {
             base.Awake();
 
             backButton.onClick.AddListener(OnClickBackButton);
+            fireButton.onClick.AddListener(OnClickFireButton);
+        }
+
+        private void Start()
+        {
+            if (dropCoverManager == null)
+            {
+                dropCoverManager = FindObjectOfType<DropCoverManager>();
+
+                if (dropCoverManager == null)
+                {
+                    Debug.LogWarning("Drop cover manager was not found for DropCoverBottomWidget");
+                    return;
+                }
+            }
+
+            titleText.text = "Drop Cover";
+            descriptionText.text = "Drop a block of full cover on the selected cell";
         }
 
         public override bool CanOpen()
         {
-            return _playerReferences.ActiveAllyController.ActiveAlly.Actions.CanUseAction(ActionType);
+            return AirSupportManager.Instance.Actions.CanUseAction(ActionType);
         }
 
         private void OnClickBackButton()
         {
             BottomWidgetManager.Instance.Show(EBottomWidget.AirSupportBase);
+        }
+
+        private void OnClickFireButton()
+        {
+            Cell target = AirSupportManager.Instance.GetHoveredCell();
+            if (target == null)
+            {
+                // TODO hint manager
+                Debug.LogWarning("Drop cover target cell was null");
+                return;
+            }
+            
+            dropCoverManager.HandleDropCover(target);
         }
     }
 }
